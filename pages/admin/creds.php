0863cbe69cdafc26dd8ca509e79370c118582d2d
Corey McCown <coreymccown13@gmail.com>
2016-01-10 14:31:44 -0500

    Create creds.php
    Credential storage file used throughout the administration panel

---
+++ pages/admin/creds.php
@@ -0,0 +1,5 @@
+<?php
+$username = ""; //Replace with the username you prefer
+$password = ""; //Replace with md5 hash of preferred password. If you want it in plaintext, use hash("md5","plaintextpassword");
+$hash = ""; //Replace with a random hash of your choice. I use a 256bit random hash generated through KeePass.
+?>
